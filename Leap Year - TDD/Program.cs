8f4f958d156fe40bb5ac797741763d3e563a8d5a
mark <markpowell1991@hotmail.co.uk>
2021-11-28 18:29:19 +0000

    initial commit

---
+++ Leap Year - TDD/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+namespace Leap_Year___TDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+
+    public class Year
+    {
+
+        private int _year { get; }
+
+        public Year(int year)
+        {
+            _year = year;
+        }
+
+
+        public bool IsLeapYear()
+        {
+
+            if (IsDivisibleBy(100))
+            {
+                //a year is a leap year if divisable by 400
+                if (IsDivisibleBy(400))
+                {
+                    return true;
+                }
+                //a year is NOT a leap year if divisable by 100 but not by 400
+                return false;
+            }
+            //a year is a leap year if divisable by 4
+            if (IsDivisibleBy(4))
+            {
+                return true;
+            }
+            //a year is NOT a leap year if not divisable by 4
+            return false;
+
+        }
+
+        //A Method created to minimise code which would be duplicated multiple times.
+        private bool IsDivisibleBy(int value)
+        {
+            return _year % value == 0;
+        }
+
+    }
+
+
+}
---
+++ LeapYearTests/LeapYearTests.cs
@@ -0,0 +1,45 @@
+﻿using Leap_Year___TDD;
+using NUnit.Framework;
+using System;
+
+namespace LeapYearTests
+{
+    [TestFixture]
+    public class LeapYearTests
+    {
+
+        //A test has been given to test that each of the requirements of a leap year have been met. 
+        [Test]
+        public void False_if_Not_Divisable_By_4()
+        {
+            Assert.IsFalse(IsLeapYear(1997));
+        }
+
+        [Test]
+        public void True_If_Divisable_By_4()
+        {
+            Assert.IsTrue(IsLeapYear(1996));
+        }
+
+        [Test]
+        public void True_if_Divisable_By_400()
+        {
+            Assert.IsTrue(IsLeapYear(1600));
+        }
+
+        [Test]
+        public void False_If_Divisable_By_100_But_Not_400()
+        {
+            Assert.IsFalse(IsLeapYear(1800));
+        }
+
+      //a method created to minimise code that has been duplicated across each of the tests.
+        private static bool IsLeapYear(int year)
+        {
+            return new Year(year).IsLeapYear();
+        }
+
+    }
+
+    
+}
